2819c576779716cbb621ae4098750ac913d5b4c3
kkassim1 <barry_kwam20@yahoo.com>
2023-06-10 14:06:51 -0700

    max num in slice

--- main.go
+++ main.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-
 	sl := []int{1, 2, 3, 55, 3, 222}
 	t := maxNumb(sl)
-
 	fmt.Println(t)
 }
 
 func maxNumb(slic []int) int {
-	keeptrack := -1
+	keeptrack := math.MinInt64
 
 	for i := 0; i < len(slic); i++ {
 		if slic[i] > keeptrack {
 			keeptrack = slic[i]
 		}
 	}
+
 	return keeptrack
 }
