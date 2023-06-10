9e3db652053fd7f0b6bb35f6e67ac96249f3e076
kkassim1 <barry_kwam20@yahoo.com>
2023-06-10 14:03:21 -0700

    max num in slice

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	sl := []int{1, 2, 3, 55, 3, 222}
+	t := maxNumb(sl)
+
+	fmt.Println(t)
+}
+
+func maxNumb(slic []int) int {
+	keeptrack := -1
+
+	for i := 0; i < len(slic); i++ {
+		if slic[i] > keeptrack {
+			keeptrack = slic[i]
+		}
+	}
+	return keeptrack
+}
