abc0af651fc8f3947649dbfb6818797be429ded8
kkassim1 <barry_kwam20@yahoo.com>
2023-07-05 22:41:38 -0700

    sdl start

--- games/kwamgamesgo.go
+++ games/kwamgamesgo.go
@@ -1,37 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
 
-// todo insert and delete page
+	"github.com/veandco/go-sdl2/sdl"
+)
 
-type storypage struct {
-	text string
-	next *storypage
-}
-
-func (p *storypage) playstory() {
-
-	for p != nil {
-		fmt.Println(p.text)
-		p = p.next
+func run() error {
+	if err := sdl.Init(0x00000020); // Replace 0x00000020 with the appropriate flag value
+	err != nil {
+		return fmt.Errorf("failed to initialize SDL: %v", err)
 	}
-}
+	defer sdl.Quit()
 
-func (p *storypage) addtoend(textToBeAddedToPage string) {
-
-	for p.next != nil {
-		p = p.next
+	// Create a window
+	window, err := sdl.CreateWindow("SDL Window", -1, -1, 800, 600, 0x00000004)
+	if err != nil {
+		return fmt.Errorf("failed to create window: %v", err)
 	}
-	p.next = &storypage{textToBeAddedToPage, nil}
+	defer window.Destroy()
 
+	// Create a renderer
+	renderer, err := sdl.CreateRenderer(window, -1, 0x00000002)
+	if err != nil {
+		return fmt.Errorf("failed to create renderer: %v", err)
+	}
+	defer renderer.Destroy()
+
+	// Main loop
+	running := true
+	for running {
+		// Event handling
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				running = false
+				break
+			}
+		}
+
+		// Rendering
+		renderer.Clear()
+		// Add your rendering code here
+		renderer.Present()
+	}
+
+	return nil
 }
 
 func main() {
-
-	f := storypage{"page 1", nil}
-	f.addtoend("page 2")
-	f.addtoend("page 3")
-
-	f.playstory()
-
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module structygo.com/m
 
 go 1.20
+
+require github.com/veandco/go-sdl2 v0.4.35
